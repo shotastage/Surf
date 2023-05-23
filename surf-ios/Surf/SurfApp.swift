55ad75f2ff7d5a5c25730f3759921e1b677e5c51
shotastage <shota.shimazu@magicalsoft.app>
2023-05-23 21:41:26 +0900

    add ios base proj

---
+++ surf-ios/Surf/SurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfApp.swift
+//  Surf
+//
+//  Created by Shota Shimazu on 2023/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
