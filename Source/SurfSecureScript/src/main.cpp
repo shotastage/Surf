44669e0f50691501ef08178d869bcdfac7415ccc
shotastage <shota.shimazu@magicalsoft.app>
2024-03-18 00:38:16 +0900

    update to be compatible cmake

---
+++ Source/SurfSecureScript/src/main.cpp
@@ -0,0 +1,14 @@
+#include "Lexer.hpp"
+#include <iostream>
+
+int main() {
+    std::string input = "func functionName(message: String): Int64 { ret 1 }";
+    Lexer lexer(input);
+    auto tokens = lexer.tokenize();
+
+    for (const auto& token : tokens) {
+        std::cout << "Token: " << static_cast<int>(token.type) << ", Value: " << token.value << std::endl;
+    }
+
+    return 0;
+}
