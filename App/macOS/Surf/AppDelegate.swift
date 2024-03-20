1ebdd145c2f1babaa9d26187623c061a312e631a
shotastage <shota.shimazu@magicalsoft.app>
2024-03-20 19:14:03 +0900

    add logger

---
+++ App/macOS/Surf/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Surf
+//
+//  Created by Shota Shimazu on 2024/03/20.
+//
+
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        TSLogger.info("Application launched.")
+    }
+}
