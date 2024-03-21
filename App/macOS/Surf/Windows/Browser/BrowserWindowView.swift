f12acb032545008020941542e62b136c96c1a7dc
shotastage <shota.shimazu@magicalsoft.app>
2024-03-21 23:13:32 +0900

    update browser window view

--- App/macOS/Surf/Windows/Browser/BrowserWindowView.swift
+++ App/macOS/Surf/Windows/Browser/BrowserWindowView.swift
@@ -14,10 +14,18 @@ struct BrowserWindowView: View {
     @Environment(\.window) var window
 
     @State private var currentURL = "https://github.com/shotastage/Surf"
+    @State private var urlStr = "https://github.com/shotastage/Surf"
 
     var body: some View {
         VStack {
-            TextField("Paste URL here...", text: $currentURL)
+            TextField("Paste URL here...", text: $urlStr)
+                .onChange(of: urlStr) { oldValue, newValue in
+                    print("Url bar string changed: \(newValue)")
+                }
+                .onSubmit {
+                    print("PressReturn Key")
+                    currentURL = urlStr
+                }
             SafariWebView(mesgURL: currentURL)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
