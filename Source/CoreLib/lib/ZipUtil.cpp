9fd26a225145b66a385fb170272ee24471e5eb03
shotastage <shota.shimazu@magicalsoft.app>
2024-04-06 09:04:14 +0900

    revise

--- Source/CoreLib/lib/ZipUtil.cpp
+++ Source/CoreLib/lib/ZipUtil.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 
 string ZipUtil::extract(string &data) {
+  gzFile file = gzopen("compressed.zip", "rb");
+  if (file == NULL) {
+    cerr << "Can not open file." << endl;
+    return 1;
+  }
+
+  struct gz_header header;
+  gzread(file, &header, sizeof(header));
   return "__HELLO_DATA__";
 }
 
